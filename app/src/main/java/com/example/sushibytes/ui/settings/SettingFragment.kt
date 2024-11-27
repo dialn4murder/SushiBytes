379d4261ae6af8811428b74bff45b3323e6cec60
niah-stack <dylanchampion596@hotmail.com>
2024-11-27 00:30:54 +0000

    Simple light and dark mode, does not save the state

--- app/src/main/java/com/example/sushibytes/ui/settings/SettingFragment.kt
+++ app/src/main/java/com/example/sushibytes/ui/settings/SettingFragment.kt
@@ -1,10 +1,14 @@
 package com.example.sushibytes.ui.settings
 
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.sushibytes.databinding.FragmentSettingsBinding
@@ -32,11 +36,30 @@ class SettingFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Creates a listener for the button
+        binding.darkMode.setOnClickListener{
+            // Checks what the theme is
+            if (isDarkThemeOn())
+                // Changes to light mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            else {
+                // Changes to dark mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun isDarkThemeOn(): Boolean {
+        // Returns the light / dark mode state
+        return resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES
     }
 }
